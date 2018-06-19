20f100fc78eb3c5e9876a93f9d55429cc3cb8976
Siddharth Nayak <siddharthnayak98@gmail.com>
2018-06-19 15:17:49 +0200

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -1,3 +1,6 @@
+# insert package name in <package name> and proxy address in <proxy name>
+# eg: pip install tensorflow --proxy proxy-usa.blabla.net
+
 pip install <package name> --proxy <proxy name>
 
 conda config --set proxy_servers.http http://<proxy name>
